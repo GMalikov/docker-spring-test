cf2872d7529e9ce0206674ecf63fe93ac316d165
GMalikov <lemartis@gmail.com>
2020-04-12 17:25:45 +0300

    Buzzers for logging test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 COPY target/*.jar app.jar
-RUN mkdir logs && touch /logs/spring-boot-logger.log
-RUN ln -sf /dev/stdout /logs/spring-boot-logger.log
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.restservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,17 +11,20 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, %s! From buzzer %s!";
+    private static final String message_template = "%s from buzzer %s!";
+    @Value("${buzzer:unknown}")
+    private String buzzerId;
     private final AtomicLong counter = new AtomicLong();
     Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
-        logger.trace("A TRACE Message");
-        logger.debug("A DEBUG Message");
-        logger.info("An INFO Message");
-        logger.warn("A WARN Message");
-        logger.error("An ERROR Message");
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        logger.trace(String.format(message_template, "A TRACE Message", buzzerId));
+        logger.debug(String.format(message_template, "A TRACE Message", buzzerId));
+        logger.info(String.format(message_template, "A TRACE Message", buzzerId));
+        logger.warn(String.format(message_template, "A TRACE Message", buzzerId));
+        logger.error(String.format(message_template, "A TRACE Message", buzzerId));
+        return new Greeting(counter.incrementAndGet(), String.format(template, name, buzzerId));
     }
 }

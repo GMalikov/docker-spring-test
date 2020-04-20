330c7d268faaf2de8c60f41c40f0ae8001a68cbf
Igor <ikozar@gmail.com>
2020-04-20 12:10:53 +0300

    upd jars

--- kube/app-ns.sh
+++ kube/app-ns.sh
@@ -1,17 +1,20 @@
+# params 1 - namespace, 2 - git branch or commit
+
 kubectl delete configmap --all -n $1
 kubectl delete pod --all -n $1
 kubectl delete svc --all -n $1
 
-kubectl create configmap config-dev1  -n $1 \
+kubectl create configmap config-app1  -n $1 \
     --from-file=conf/application.properties \
-    --from-file=conf/application-dev1.properties \
+    --from-file=conf/application-app1.properties \
     --from-file=conf/$1/application-spec.properties
 
-kubectl create configmap config-dev2  -n $1 \
+kubectl create configmap config-app2  -n $1 \
     --from-file=conf/application.properties \
     --from-file=conf/$1/
 
-kubectl apply -f kube/app2.yaml -n $1
+kubectl apply -f kube/pod.yaml -n $1
+kubectl apply -f kube/svc.yaml -n $1
 
 cat <<EOF > kube/kustomization.yaml
 resources:
@@ -22,7 +25,7 @@ patchesJson6902:
     version: v1beta1
     kind: Ingress
     name: bright-ingress
-  path: $1/patch.yaml
+  path: ingress/$1/patch-host.yaml
 EOF
 
 kubectl apply -k kube -n $1
